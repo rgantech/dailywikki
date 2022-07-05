20da284424707402b3cb3d9afb445c7600832d23
rgantech <rohit.gautam@antiersolutions.com>
2022-07-05 10:46:24 +0530

    fix date issue add 0 in front of date

--- src/Api/HomeApi.js
+++ src/Api/HomeApi.js
@@ -1,3 +1,4 @@
 const today = new Date();
-const month = ("0" + Math.floor(today.getMonth()+1)).slice(-2) ;   
-export const forHomeurl = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${today.getFullYear()}/${month}/${Math.floor(today.getDate())}`
+const month = ("0" + Math.floor(today.getMonth()+1)).slice(-2);
+const date = ("0" + Math.floor(today.getDate())).slice(-2) 
+export const forHomeurl = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${today.getFullYear()}/${month}/${date}`
